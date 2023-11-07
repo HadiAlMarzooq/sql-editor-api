36e5c20c716df520b2191d0b9c6c3f2d53b00f17
HadiAlMarzooq <ahcraft1@gmail.com>
2023-11-07 21:07:46 +0300

    Add SQLite database support and SQL query execution endpoint

--- server/api/index.js
+++ server/api/index.js
@@ -1,23 +1,44 @@
 // index.js
 const express = require('express');
 const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
 
-// Import any other routes or middleware
-app.use('/api', require('./execute-sql'));
+// Middleware to parse the incoming request body as JSON
+app.use(express.json());
 
-// You might have other routes here
-// ...
+// Add a separate OPTIONS handler
+app.options('/execute-sql', cors()); // Enable pre-flight request for POST request
 
-// Start server
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// Endpoint to execute SQL query
+app.post('/execute-sql', (req, res) => {
+  const sql = req.body.sql.trim();
+
+  // Connect to SQLite database
+  const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: 'Failed to connect to the database.' });
+    }
   });
-}
+
+  // Validate that the SQL query is a read operation
+  if (/^(INSERT|UPDATE|DELETE)/i.test(sql)) {
+    return res.status(400).json({ error: 'Write operations are not allowed.' });
+  }
+
+  // Execute SQL query
+  db.all(sql, [], (err, rows) => {
+    db.close(); // Close the database connection
+
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.json({ rows });
+  });
+});
 
 module.exports = app;
